a0801ceba0d6ab9ea870e00c3f39ff4d2cbd08e0
jscherman <jonischerman@gmail.com>
2018-09-23 13:16:13 -0300

    Leves cambios a algunos graficos y algun refactor

--- tp1/src/exp/exp_base.py
+++ tp1/src/exp/exp_base.py
@@ -7,8 +7,8 @@ def parsear_output(output):
     sep = output.find("\n")
     return output[0:sep], output[sep+1:-1].split("\n") #p, ranks
 
-def ejecutar_con_input(input_file, p):
-    cmd = "../tp1 -o - {} {}".format(input_file, str(p))
+def ejecutar_con_args(args):
+    cmd = "../tp1 " + " ".join([str(arg) for arg in args])
     # print(cmd)
     # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode("utf-8")
     # print(output)
