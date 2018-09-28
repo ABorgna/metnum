e6be0eda0bf6dec035c3a340234685ddd2d83c1d
Agustin Borgna <agustinborgna@gmail.com>
2018-09-28 20:29:16 -0300

    correcciones labo2

--- labos/2-cholesky/src/CheckCondLU.m
+++ labos/2-cholesky/src/CheckCondLU.m
@@ -2,9 +2,10 @@ function ret = CheckCondLU (a)
     % ret será
     %   1 si cumple la condición para LU
     %   0 si no
+    epsilon = 1e-14;
     ret = 1;
     for i = 1:size(a,1  )
-      if det(a(1:i, 1:i)) == 0
+      if abs(det(a(1:i, 1:i))) < epsilon
         ret = 0;
         return
       endif
