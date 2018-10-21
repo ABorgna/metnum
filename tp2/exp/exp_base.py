faff2edcd298519c9df3a28e6d60038f93d56d06
jscherman <jonischerman@gmail.com>
2018-10-21 20:53:48 -0300

    Algunas funciones utiles para exp.

---
+++ tp2/exp/exp_base.py
@@ -0,0 +1,29 @@
+# import os
+# import shutil
+
+import subprocess
+import time
+
+def parsear_output(output):
+    sep = "---------------- Testing -----------------"
+    output = output[output.find(sep) + len(sep) + 1:-1].split("\n") # ['param: val', ..., 'param: val']
+    output = [elem.split(": ") for elem in output]                  # [['param','val'], ..., ['param', 'val]]
+    output = dict(output)                                           # {'param': val', ..., 'param': val}
+    return output
+
+def ejecutar_con_args(args):
+    """ args: diccionario """
+
+    cmd = "../tp2 " + " ".join("{} {}".format(key, val) for (key, val) in args.items())
+    print(cmd)
+    return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode("utf-8")
+
+def tomar_tiempo_ejecucion_con_args(args):
+    before = time.time_ns()
+    ejecutar_con_args(args)
+    after = time.time_ns()
+    return after - before
+
+
+
+
