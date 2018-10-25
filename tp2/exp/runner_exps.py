b60cffdba0912cc8e80a1bd1ba9d6bca51721acb
jscherman <jonischerman@gmail.com>
2018-10-25 08:42:50 -0300

    Datos de algunos exp

---
+++ tp2/exp/runner_exps.py
@@ -0,0 +1,12 @@
+import exp_variando_k_alpha_invertedknn
+import exp_tiempos_variando_alpha_k
+
+try:
+    exp_tiempos_variando_alpha_k()
+except:
+    print("ERROR: exp_tiempos_variando_alpha_k")
+
+try:
+    exp_variando_k_alpha_invertedknn()
+except:
+    print("ERROR: exp_variando_k_alpha_invertedknn")
