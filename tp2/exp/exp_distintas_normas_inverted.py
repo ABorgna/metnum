a8acc7900b1e5171a659bc992d610c48e196d697
jdsinger <singer.jeess@gmail.com>
2018-10-26 00:09:29 -0300

    agregadas algunas notebooks

--- tp2/exp/exp_distintas_normas_inverted.py
+++ tp2/exp/exp_distintas_normas_inverted.py
@@ -5,24 +5,24 @@ from exp_base import *
 
 def escribir_resultados_en_archivo(res, resultado, tiempo_ns, norma):
     tiempo_ns = res["time-testing"][:res["time-testing"].find("ms")]
-    resultado.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(res["accuracy"], res["alpha"], res["k"],res["countEntries"], res["recall"], res["falseP"], res["falseN"], res["trueN"], res["trueP"],tiempo_ns, norma))
+    resultado.write("{},{},{},{},{},{},{},{},{},{}\n".format(res["accuracy"], res["k"],res["countEntries"], res["recall"], res["falseP"], res["falseN"], res["trueN"], res["trueP"],tiempo_ns, norma))
 
 def ejecutar_y_escribir_resultado_variando_norma(exp_args):
     cant_normas = exp_args["CANT_NORMAS"]
-    
+    cant_k = 100
     min_alpha = exp_args["MIN_ALPHA"]
     max_alpha = exp_args["MAX_ALPHA"]
-    resultado = open("./res/exp_distintas_normas"+str(cant_normas)+".csv", "w")
-    resultado.write("accuracy,alpha,k,testing entries,recall,falseP,falseN,trueN,trueP,tiempo,norma\n") # header
-    for alpha in list(numpy.linspace(min_alpha, max_alpha, exp_args["CANT_ALPHA"]))[1:-1]:
+    resultado = open("./res/exp_distintas_normas_inverted"+str(cant_normas)+".csv", "w")
+    resultado.write("accuracy,k,testing entries,recall,falseP,falseN,trueN,trueP,tiempo,norma\n") # header
+    for k in range(1, cant_k, 5):
         for norm in range(1, cant_normas):
             program_args = {"--vocabulary": exp_args["VOCAB_FILE"],
                             "-t": exp_args["TRAINING_FILE"],
                             "-q": exp_args["TESTING_FILE"],
                             "-m": exp_args["METHOD_NUMBER"],
+                            "-k" : k,
                             "--maxVocabFreq" : 0.1,
                             "-n" : norm,
-                            "-a" : alpha,
                             "--train-entries": exp_args["NUMBER_OF_TRAINING_ENTRIES"],
                             "--test-entries": exp_args["NUMBER_OF_TESTING_ENTRIES"],
                 "--quiet": ""}
@@ -40,8 +40,8 @@ def ejecutar_y_escribir_resultado_variando_norma(exp_args):
                         "-t": exp_args["TRAINING_FILE"],
                         "-q": exp_args["TESTING_FILE"],
                         "-m": exp_args["METHOD_NUMBER"],
+                        "-k" : k,
                         "--maxVocabFreq" : 0.1,
-                        "-a" : alpha,
                         "-n" : "chi2",
                         "--train-entries": exp_args["NUMBER_OF_TRAINING_ENTRIES"],
                         "--test-entries": exp_args["NUMBER_OF_TESTING_ENTRIES"],
@@ -60,9 +60,9 @@ def ejecutar_y_escribir_resultado_variando_norma(exp_args):
                         "-t": exp_args["TRAINING_FILE"],
                         "-q": exp_args["TESTING_FILE"],
                         "-m": exp_args["METHOD_NUMBER"],
+                        "-k" : k,
                         "-n" : "inf",
                         "--maxVocabFreq" : 0.1,
-                        "-a" : alpha,
                         "--train-entries": exp_args["NUMBER_OF_TRAINING_ENTRIES"],
                         "--test-entries": exp_args["NUMBER_OF_TESTING_ENTRIES"],
             "--quiet": ""}
@@ -88,9 +88,9 @@ exp_args = {"VOCAB_FILE": "../data/vocab.csv",
             "--maxVocabFreq" : 0.1,
             "TRAINING_FILE": "../data/imdb_tokenized.csv",
             "TESTING_FILE": "../data/imdb_tokenized.csv",
-            "METHOD_NUMBER": 2,
+            "METHOD_NUMBER": 3,
             "NUMBER_OF_TRAINING_ENTRIES": 2000,
-            "NUMBER_OF_TESTING_ENTRIES": 5000}
+            "NUMBER_OF_TESTING_ENTRIES": 25000}
 
 print("Ejecutando ahora ")
 ejecutar_y_escribir_resultado_variando_norma(exp_args)
