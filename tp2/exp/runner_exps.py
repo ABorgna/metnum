bbbea88abbf9b6a824a6ffe3d5003934c197eda8
jscherman <jonischerman@gmail.com>
2018-10-27 16:27:13 -0300

    Nuevos datos y nuevo experimento en exp-notebook: tiempos y accuracy variando max_vocabs_freq para distintos metodos

--- tp2/exp/runner_exps.py
+++ tp2/exp/runner_exps.py
@@ -1,36 +1,44 @@
-try:
-    import exp_tiempos_variando_alpha_k
-except:
-    print("ERROR: exp_tiempos_variando_alpha_k")
-
-print("----------------------1-----------------------")
-
-try:
-    import exp_pca_n_vs_sin_pca
-except:
-    print("ERROR: exp_pca_n_vs_sin_pca")
-
-print("--------------------2-------------------------")
-
-try:
-    import exp_distintas_normas_inverted
-except:
-    print("ERROR: exp_distintas_normas_inverted")
-
-print("--------------------3-------------------------")
-
-try:
-    import exp_distintas_normas
-except:
-    print("ERROR: exp_distintas_normas")
-
+# try:
+#     import exp_tiempos_variando_alpha_k
+# except:
+#     print("ERROR: exp_tiempos_variando_alpha_k")
+#
+# print("----------------------1-----------------------")
+#
+# try:
+#     import exp_pca_n_vs_sin_pca
+# except:
+#     print("ERROR: exp_pca_n_vs_sin_pca")
+#
+# print("--------------------2-------------------------")
+#
+# try:
+#     import exp_distintas_normas_inverted
+# except:
+#     print("ERROR: exp_distintas_normas_inverted")
+#
+# print("--------------------3-------------------------")
+#
+# try:
+#     import exp_distintas_normas
+# except:
+#     print("ERROR: exp_distintas_normas")
+#
+#
+# print("----------------------4-----------------------")
+#
+# try:
+#     import exp_training_entries_vs_accuracy_vs_tiempo
+# except:
+#     print("ERROR: exp_training_entries_vs_accuracy_vs_tiempo")
 
 print("----------------------4-----------------------")
 
 try:
-    import exp_training_entries_vs_accuracy_vs_tiempo
+    import exp_tiempos_variando_max_vocabs
 except:
-    print("ERROR: exp_training_entries_vs_accuracy_vs_tiempo")
+    print("ERROR: exp_tiempos_variando_max_vocabs")
+
 
 
 print("----------------------5-----------------------")
@@ -40,7 +48,8 @@ try:
 except:
     print("ERROR: exp_variando_k_alpha_invertedknn")
 
-
+import subprocess
+subprocess.check_output("shutdown -h now", stderr=subprocess.STDOUT, shell=True).decode("utf-8")
 
 
 
