537fa2c46b7c430c96d83cda9fed08bf87406440
Teo Freund <tfreund95@gmail.com>
2018-11-29 16:10:55 -0300

    Exp runner

---
+++ tp3/conversor_csv/exp_runner.py
@@ -0,0 +1,75 @@
+from utils import *
+from csv_converter import convertImgs
+import numpy
+import shutil
+import json
+
+# LEEEEMEEEEEEEEE (para correr)
+# 1. borrar imgs en imgs_input
+# 2. generar imgs a correr en imgs_input: python csv_converter.py ../data/ imgs_input/ .png 32
+# los archivos tienen que ser de la forma "nombre-tamaño.csv" (el conversor los genera asi por default)
+# 2. correr y buscar resultados del exp. en carpeta resultados =)
+
+
+
+def listar_archivos_en_carpeta(path):
+    filenames = []
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            filenames = filenames + [filename]
+    return filenames
+
+def ejecutar_y_escribir_resultado(exp_args):
+    print("\n-----------CREATE IMAGES----------------")
+
+    if not os.path.exists('../imgs_input'):
+        os.mkdir('../imgs_input')
+    for t in exp_args['n_cells']:
+        convertImgs('../../data', '../imgs_input', '.png', tam = t)
+
+
+    res = init_resultados()
+    for img in exp_args['imgs']:
+        for tam in exp_args['n_cells']:
+            input_img = '../imgs_input/' + img + '-' + str(tam) + '.csv'
+            for ray_type in exp_args['rays_types']:
+                for ray_cnt in exp_args['rays_cnt']:
+                    for lsq in exp_args['lsq']:
+                        for error_type in exp_args['error_type']:
+                            for error_std in exp_args['error_std']:
+                                for rep in range(exp_args['reps']):
+                                    output_img = '-'.join(map(str, 
+                                        [img, tam, ray_type, ray_cnt, lsq, error_type, error_std, rep]))
+                                    output_img += '.csv'
+                                    program_args = {'-n' : tam,
+                                                    '--ray-type': ray_type,
+                                                    '--ray-count': ray_cnt,
+                                                    '-l' : lsq,
+                                                    '-e' : error_type,
+                                                    '-E' : error_std,
+                                                    '--no-cache':''}
+
+                                    output = ejecutar_tp(input_img, output_img, program_args)
+                                    print(output)
+                                    parsed_output = parsear_output(output)
+
+                                    value_psnr = psnr(output_img, input_img)
+                                    escribir_resultados_en_archivo(res,
+                                        exp_args['nombre'], img, tam, ray_type, ray_cnt, lsq,
+                                        error_type, error_std, parsed_output, value_psnr)
+
+
+
+    res.close()
+
+
+
+
+with open("exp.json", "r") as read_file:
+    exp_args = json.load(read_file)
+exp_args['rays_cnt'] = list(range(exp_args['rays_cnt']['from'], exp_args['rays_cnt']['to'] + 1, exp_args['rays_cnt']['step']))
+print(exp_args)
+
+print("Ejecutando exp...")
+ejecutar_y_escribir_resultado(exp_args)
+print("Listo!")
--- tp3/conversor_csv/utils.py
+++ tp3/conversor_csv/utils.py
@@ -15,9 +15,9 @@ def ejecutar(cmd):
     print(cmd)
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode("utf-8")
 
-def ejecutar_tp(dict_args):
+def ejecutar_tp(input, output, dict_args):
 
-    cmd = "../tp3 " + " ".join("{} {}".format(key, val) for (key, val) in dict_args.items())
+    cmd = "../../tp3 " +" {} {} ".format(input, output) + " ".join("{} {}".format(key, val) for (key, val) in dict_args.items())
     print(cmd)
 
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode("utf-8")
@@ -32,6 +32,7 @@ def mse(img1, img2):
 
     rows = img1.shape[0]
     cols = img1.shape[1]
+
 
     val = (img1 - img2).apply(lambda x : x**2).values.sum()
 
@@ -55,6 +56,42 @@ def psnr(img1_rel_path, img2_rel_path):
     else:
         return 20*math.log10(MAX/math.sqrt(mse_val))
 
+def init_resultados(nombre = 'exp_res.csv'):
+    if not os.path.isfile(nombre):
+        resultado = open("./" + nombre, "w")
+        header = 'nombre_exp,nombre_img,n_cells,ray_type,ray_cnt,lsq,error,error_std,tiempo_lsq,cant_autovalores,psnr'
+        resultado.write(header + "\n")
+    else:
+        resultado = open(nombre, 'a')
+    return resultado
 
+def escribir_resultados_en_archivo(resultado, nombre, img, n_cells, ray_type, ray_cnt, lsq, error, error_std, output, psnr):
+    tiempo_ms = output["time-lsq"][:output["time-lsq"].find("ms")]
+    line = ''
+    line += str(nombre)
+    line += ','
+    line += str(img)
+    line += ','
+    line += str(n_cells)
+    line += ','
+    line += str(ray_type)
+    line += ','
+    line += str(ray_cnt)
+    line += ','
+    line += str(lsq)
+    line += ','
+    line += str(error)
+    line += ','
+    line += str(error_std)
+    line += ','
+    line += str(tiempo_ms)
+    line += ','
+    line += str(0) # TODO: imprimir autovalores desde el tp3, y parsear
+    line += ','
+    line += str(psnr)
+    line += '\n'
+
+    resultado.write(line)
+    print(">> {}".format(line))
 
 # print("PSNR: " + str(psnr('../data/phantom3.csv', '../data/phantom3.csv')))
